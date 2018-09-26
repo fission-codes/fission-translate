65d2410c392633a14f75d1ca940d378a23adeb57
Jenn Cooper <cooper.jennl@gmail.com>
2018-09-25 18:09:58 -0700

    Include coverage

--- test/LocalizationPreferencesTests.js
+++ test/LocalizationPreferencesTests.js
@@ -3,23 +3,27 @@ var Localization = artifacts.require("./Localization.sol");
 
 contract('LocalizationPreferencesTests', async (accounts) => {
   let defaultLocalizationInstance;
-  let frenchLocalizationInstance;
   let localizationPreferencesInstance;
 
   beforeEach('setup', async () => {
     defaultLocalizationInstance = await Localization.deployed();
-    defaultLocalizationInstance.set(web3.toHex('0x01'), "Success");
-    defaultLocalizationInstance.set(web3.toHex('0x00'), "Failure");
-
-    frenchLocalizationInstance = await Localization.new();
-    frenchLocalizationInstance.set(web3.toHex('0x01'), "Succès");
-    frenchLocalizationInstance.set(web3.toHex('0x00'), "Échec");
+    await defaultLocalizationInstance.set(web3.toHex('0x01'), "Success");
+    await defaultLocalizationInstance.set(web3.toHex('0x00'), "Failure");
 
     localizationPreferencesInstance = await LocalizationPreferences.new(defaultLocalizationInstance.address);
-    localizationPreferencesInstance.set(frenchLocalizationInstance.address);
+  });
+
+  it("should use the default localization when none has been set", async () => {
+    const result = await localizationPreferencesInstance.get(web3.toHex('0x01'), {from: accounts[3]});
+    expect(result).to.eql([true, "Success"]);
   });
 
   it("should get text for a given code", async () => {
+    let frenchLocalizationInstance = await Localization.new();
+    await frenchLocalizationInstance.set(web3.toHex('0x01'), "Succès");
+    await frenchLocalizationInstance.set(web3.toHex('0x00'), "Échec");
+
+    await localizationPreferencesInstance.set(frenchLocalizationInstance.address);
     const result = await localizationPreferencesInstance.get(web3.toHex('0x01'));
 
     expect(result).to.eql([true, "Succès"]);
@@ -27,20 +31,26 @@ contract('LocalizationPreferencesTests', async (accounts) => {
 
   it("should set a new localization", async () => {
     spanishLocalizationInstance = await Localization.new();
-    spanishLocalizationInstance.set(web3.toHex('0x01'), "Éxito");
+    await spanishLocalizationInstance.set(web3.toHex('0x01'), "Éxito");
 
-    localizationPreferencesInstance.set(spanishLocalizationInstance.address);
+    await localizationPreferencesInstance.set(spanishLocalizationInstance.address);
 
     const result = await localizationPreferencesInstance.get(web3.toHex('0x01'));
 
     expect(result).to.eql([true, "Éxito"]);
   });
 
+  it("should return false and an empty string if no code matches any localizations", async () => {
+    const result = await localizationPreferencesInstance.get(web3.toHex('0x12'));
+
+    expect(result).to.eql([false, ""]);
+  });
+
   it("should fallback to the default localization", async () => {
     spanishLocalizationInstance = await Localization.new();
-    spanishLocalizationInstance.set(web3.toHex('0x01'), "Éxito");
+    await spanishLocalizationInstance.set(web3.toHex('0x01'), "Éxito");
 
-    localizationPreferencesInstance.set(spanishLocalizationInstance.address);
+    await localizationPreferencesInstance.set(spanishLocalizationInstance.address);
 
     const result = await localizationPreferencesInstance.get(web3.toHex('0x00'));
 
