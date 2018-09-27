21fd5b75b864b2a0342d7672cc343347dcd4df62
Jenn Cooper <cooper.jennl@gmail.com>
2018-09-27 05:50:27 -0700

    More declarative test descriptions

--- test/LocalizationPreferencesTests.js
+++ test/LocalizationPreferencesTests.js
@@ -13,12 +13,12 @@ contract('LocalizationPreferencesTests', async (accounts) => {
     localizationPreferencesInstance = await LocalizationPreferences.new(defaultLocalizationInstance.address);
   });
 
-  it("should use the default localization when none has been set", async () => {
+  it("uses the default localization when none has been set", async () => {
     const result = await localizationPreferencesInstance.get(web3.toHex('0x01'), {from: accounts[3]});
     expect(result).to.eql([true, "Success"]);
   });
 
-  it("should get text for a given code", async () => {
+  it("gets text for a given code", async () => {
     let frenchLocalizationInstance = await Localization.new();
     await frenchLocalizationInstance.set(web3.toHex('0x01'), "Succès");
     await frenchLocalizationInstance.set(web3.toHex('0x00'), "Échec");
@@ -29,7 +29,7 @@ contract('LocalizationPreferencesTests', async (accounts) => {
     expect(result).to.eql([true, "Succès"]);
   });
 
-  it("should set a new localization", async () => {
+  it("sets a new localization", async () => {
     spanishLocalizationInstance = await Localization.new();
     await spanishLocalizationInstance.set(web3.toHex('0x01'), "Éxito");
 
@@ -40,13 +40,13 @@ contract('LocalizationPreferencesTests', async (accounts) => {
     expect(result).to.eql([true, "Éxito"]);
   });
 
-  it("should return false and an empty string if no code matches any localizations", async () => {
+  it("returns false and an empty string if no code matches any localizations", async () => {
     const result = await localizationPreferencesInstance.get(web3.toHex('0x12'));
 
     expect(result).to.eql([false, ""]);
   });
 
-  it("should fallback to the default localization", async () => {
+  it("falls back to the default localization", async () => {
     spanishLocalizationInstance = await Localization.new();
     await spanishLocalizationInstance.set(web3.toHex('0x01'), "Éxito");
 
--- test/LocalizationTests.js
+++ test/LocalizationTests.js
@@ -8,13 +8,13 @@ contract('LocalizationTests', async (accounts) => {
     await localizationInstance.set(web3.toHex('0x01'), "Success");
   });
 
-  it("should get text for a given code", async () => {
+  it("gets text for a given code", async () => {
     const result = await localizationInstance.textFor(web3.toHex('0x01'));
 
     expect(result).to.equal("Success");
   });
 
-  it("should set text for a given code", async () => {
+  it("sets text for a given code", async () => {
     await localizationInstance.set(web3.toHex('0x00'), "Failure");
 
     const result = await localizationInstance.textFor(web3.toHex('00'));
