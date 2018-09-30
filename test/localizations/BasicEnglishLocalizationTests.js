319cfab9de2e899667abd9541b30974e19a932dd
Jenn Cooper <cooper.jennl@gmail.com>
2018-09-30 11:33:10 -0700

    Update tests

--- test/localizations/BasicEnglishLocalizationTests.js
+++ test/localizations/BasicEnglishLocalizationTests.js
@@ -1,15 +1,15 @@
-var LocalizationPreferences = artifacts.require("./LocalizationPreferences.sol");
+var BasicEnglishLocalization = artifacts.require("BasicEnglishLocalization.sol");
 
 contract("BasicEnglishLocalizationTests", async (accounts) => {
-  let localizationPreferencesInstance;
+  let basicEnglishLocalizationInstance;
 
   before("setup", async () => {
-    localizationPreferencesInstance = await LocalizationPreferences.deployed();
+    basicEnglishLocalizationInstance = await BasicEnglishLocalization.deployed();
   });
 
   it("gets the correct string for a given code", async () => {
-    const result = await localizationPreferencesInstance.get(web3.utils.toHex("0x00"));
+    const result = await basicEnglishLocalizationInstance.textFor(web3.utils.toHex("0x00"));
 
-    expect(result).to.eql({"0": true, "1": "Failure"});
+    expect(result).to.eql("Failure");
   });
 });
